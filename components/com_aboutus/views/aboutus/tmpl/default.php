5099f7f25c565fb075c09a649bfe3b5eb92f5634
cuunonhaitac <cuunonhaitac@gmail.com>
2014-08-02 22:51:35 +0700

    Fix button about us

--- components/com_aboutus/views/aboutus/tmpl/default.php
+++ components/com_aboutus/views/aboutus/tmpl/default.php
@@ -86,20 +86,13 @@ defined('_JEXEC') or die('Restricted access');
 			data-anchor-target="#slide-4"
 		>
 			<div class="hsContainer">
-				<div class="hsContent"
-					data-bottom-top="opacity: 0"
-					data-25p-top="opacity: 0"
-					data-top="opacity: 1" 
-					data-anchor-target="#slide-4"
-				>
-				<div class='tbl_button'>
-					<a href="./?option=com_content" title="Tham Gia" style="margin-left:-50px;margin-right:50px;">Tham Gia</a>
-                    <a href="https://www.facebook.com/toibalo" title="Facebook Page">Facebook Page</a>
-				</div>
-
-				</div>
+				
 			</div>
 		</div>
 	</section> 
 </main>
+	<div class='tbl_button'>
+		<a href="./?option=com_content" title="Tham Gia" style="margin-left:-50px;margin-right:50px;">Tham Gia</a>
+		<a href="https://www.facebook.com/toibalo" title="Facebook Page">Facebook Page</a>
+	</div>
 </div>
